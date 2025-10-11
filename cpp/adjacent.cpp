d2211575e9387554c4f86fda91455dc7fcb972dc
Michael <mail@michaelsjoeberg.com>
2025-10-11 20:00:15 +0700

    Create adjacent.cpp

---
+++ cpp/adjacent.cpp
@@ -0,0 +1,19 @@
+
+// find adjacent in vector
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+bool twice (int a, int b) { return a * 2 == b; }
+
+int main() {
+    std::vector<int> numbers = { 50, 40, 10, 20, 20 };
+    
+    auto iter = std::adjacent_find(numbers.begin(), numbers.end(), twice);
+    if (iter != numbers.end()) {
+        std::cout << *iter << ", " << *(iter + 1) << std::endl;
+    } else {
+        std::cout << "no adjacent pairs" << std::endl;
+    }
+}
