a04109fee13297e8b0705451982a62da9449070c
Michael <mail@michaelsjoeberg.com>
2025-10-11 21:19:13 +0700

    Create bubble.cpp

---
+++ cpp/bubble.cpp
@@ -0,0 +1,31 @@
+
+// bubble sort
+
+#include <iostream>
+#include <vector>
+
+constexpr void print_elements_in_vector(std::vector<int> numbers) {
+    for (int number : numbers) {
+        std::cout << number << std::endl;
+    }
+}
+
+int main() {
+    std::vector<int> numbers = { 14, 33, 27, 35, 10 };
+    // print unsorted
+    print_elements_in_vector(numbers); // 14 33 27 35 10
+
+    // bubble sort
+    for (int i = 0; i < numbers.size(); i++) {
+        for (int j = 0; j < numbers.size() - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                int temp = numbers[j];
+                // swap positions
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    // print sorted
+    print_elements_in_vector(numbers); // 10 14 27 33 35
+}
