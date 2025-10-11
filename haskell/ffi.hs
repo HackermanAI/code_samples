75ee0d47cfdc81d7a00678013f3d1f95832b81b7
Michael <mail@michaelsjoeberg.com>
2025-10-11 23:46:54 +0700

    Create ffi.hs

---
+++ haskell/ffi.hs
@@ -0,0 +1,24 @@
+
+-- foreign function interface
+
+{-# LANGUAGE ForeignFunctionInterface #-}
+module Main where
+
+import Prelude hiding (sin)
+
+import Foreign.C -- get the C types
+import Foreign.Ptr (Ptr,nullPtr)
+
+-- pure function
+foreign import ccall "sin" c_sin :: CDouble -> CDouble
+sin :: Double -> Double
+sin d = realToFrac (c_sin (realToFrac d))
+
+-- impure function
+foreign import ccall "time" c_time :: Ptr a -> IO CTime
+getTime :: IO CTime
+getTime = c_time nullPtr
+
+main = do
+    print . sin =<< readLn
+    print =<< getTime
