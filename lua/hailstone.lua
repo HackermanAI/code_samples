ca5b7ad59ba1e9d0c98c5aa906b049841530ecf6
Michael <mail@michaelsjoeberg.com>
2025-10-11 16:14:59 +0700

    Update hailstone.lua

--- lua/hailstone.lua
+++ lua/hailstone.lua
@@ -1,11 +1,11 @@
 
 -- Lua implementation of Collatz conjecture (hailstone sequence)
 
-function hailstone( n, print_numbers )
+function hailstone(n, print_numbers)
     local n_iter = 1
 
     while n ~= 1 do
-        if print_numbers then print( n ) end
+        if print_numbers then print(n) end
         if n % 2 == 0 then 
             n = n / 2
         else
@@ -14,20 +14,20 @@ function hailstone( n, print_numbers )
         
         n_iter = n_iter + 1
     end
-    if print_numbers then print( n ) end
+    if print_numbers then print(n) end
     
     return n_iter;
 end
 
-hailstone( 27, true )
+hailstone(27, true)
 
 max_i, max_iter = 0, 0
 for i = 1, 100000 do
-    num = hailstone( i, false )
+    num = hailstone(i, false)
     if num >= max_iter then
         max_i = i
         max_iter = num
     end
 end
 
-print( string.format( "Needed %d iterations for the number %d.\n", max_iter, max_i ) )
+print(string.format("Needed %d iterations for the number %d.\n", max_iter, max_i))
