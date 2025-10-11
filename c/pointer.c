ad2f0d42ad46eb7250c4e0519d36f1b0f6a75fb8
Michael <mail@michaelsjoeberg.com>
2025-10-11 21:55:19 +0700

    Create pointer.c

---
+++ c/pointer.c
@@ -0,0 +1,22 @@
+
+// pointer
+
+#include <stdio.h>
+#include <stddef.h>
+
+int main() {
+    int num = 5;
+    
+    // null pointer
+    int *ip = NULL;
+    printf("%p\n", ip); // 0x0 (nil)
+
+    // point to address of num
+    ip = &num;
+
+    printf("%p\n", &num); // 0x7ffdb78c2664
+    printf("%p\n", ip); // 0x7ffdb78c2664
+
+    // dereference (get value at address)
+    printf("%d\n", *ip); // 5
+}
