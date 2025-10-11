ea09768481467b3d6908dc1e9409da9a46f11738
Michael <mail@michaelsjoeberg.com>
2025-10-11 20:00:16 +0700

    Create bs.cpp

---
+++ cpp/bs.cpp
@@ -0,0 +1,17 @@
+
+// binary search
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+
+    bool found = std::binary_search(numbers.begin(), numbers.end(), 4);
+    if (found) {
+        std::cout << "found value" << std::endl;
+    } else {
+        std::cout << "not found" << std::endl;
+    }
+}
